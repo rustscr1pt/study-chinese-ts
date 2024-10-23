11268520e57a22bc0683e1469c6f7946e156f506
rust_lang_lover <rust_lang_lover@proton.me>
2024-10-23 13:23:11 +0300

    //

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import {useAppSelector} from "./redux/hooks";
-import CurrentScreen from "./structs&constants&enums/enums";
-import ManageScreenComponent from "./Screens/Manage/ManageScreen.component";
-import StudyScreenComponent from "./Screens/Study/StudyScreen.component";
+import TopWrapperComponent from "./Screens/TopWrapper/TopWrapper.component";
 
 
 const App = () => {
-    const currentScreen : CurrentScreen = useAppSelector((state) => state.currentScreen.value)
-
-    return <div>
-        {
-            currentScreen === CurrentScreen.Manage
-                ? <ManageScreenComponent/>
-                : <StudyScreenComponent/>
-        }
-    </div>
+    return <TopWrapperComponent/>
 }
 
 export default App;
---
+++ src/Screens/TopWrapper/TopWrapper.component.tsx
@@ -0,0 +1,18 @@
+import "./TopWrapper.style.sass"
+import CurrentScreen from "../../structs&constants&enums/enums";
+import ManageScreenComponent from "../Manage/ManageScreen.component";
+import StudyScreenComponent from "../Study/StudyScreen.component";
+import {useAppSelector} from "../../redux/hooks";
+const TopWrapperComponent = () => {
+    const currentScreen : CurrentScreen = useAppSelector((state) => state.currentScreenStore.value)
+
+    return <div className="top-wrapper">
+        {
+            currentScreen === CurrentScreen.Manage
+                ? <ManageScreenComponent/>
+                : <StudyScreenComponent/>
+        }
+    </div>
+}
+
+export default TopWrapperComponent;
